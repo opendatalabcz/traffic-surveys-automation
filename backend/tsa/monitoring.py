e9ece140b3395e1e93684a92819458fe43483f7e
Ondrej Pudiš <pudisond@fit.cvut.cz>
2022-04-08 12:14:32 +0200

    fix: fix iterating over configurable variables in neptune monitor

--- backend/tsa/monitoring.py
+++ backend/tsa/monitoring.py
@@ -3,14 +3,14 @@ from typing import List, Optional
 
 import neptune.new as neptune
 
-from tsa.config import config
+from tsa.config import CONFIGURABLE_VARIABLES, config
 
 
 @contextmanager
 def neptune_monitor(name: Optional[str] = None, tags: Optional[List[str]] = None):
     run = neptune.init_run(project=config.NEPTUNE_PROJECT, api_token=config.NEPTUNE_API_KEY, name=name)
     run["sys/tags"].add(tags)
-    run["parameters"] = {config[key] for key in config.CONFIGURABLE_VARIABLES}
+    run["parameters"] = {config[key] for key in CONFIGURABLE_VARIABLES}
     try:
         yield run
     finally:
