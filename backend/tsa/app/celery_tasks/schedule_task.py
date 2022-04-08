20faa9cbdd42644eaf43e571b1fb4d1bc56fadcd
Ondrej Pudiš <pudisond@fit.cvut.cz>
2022-04-08 15:12:39 +0200

    fix: update sbatch scheduling task

--- backend/tsa/app/celery_tasks/schedule_task.py
+++ backend/tsa/app/celery_tasks/schedule_task.py
@@ -14,26 +14,9 @@ async def schedule_task(task_id: int):
     task, source_file = await get_db_objects(task_id)
 
     config_file_name = _generate_config_file(task)
+    sbatch_file_name = _generate_sbatch_file(source_file, task, config_file_name)
 
-    subprocess.run(
-        [
-            "sbatch",
-            "-N 1",
-            "-c 1",
-            "-p gpu",
-            "-G 1",
-            f"-J tsa_{task.id}",
-            f"-e logs/tsa_{task.id}.err",
-            f"-o logs/tsa_{task.id}.out",
-            (
-                "singularity exec --bind ~/models:/app/models --nv"
-                f"--env='MODELS_PATH=/app/models,DATABASE_NAME={config.DB_NAME},DATABASE_URL={config.DB_URL}'"
-                "traffic-surveys-automation_master.sif"
-                f"python /app/cli/analyse.py -f ~/input/{source_file.path} -o ~/output/{task.output_path}"
-                f"-d {task.models[0]} -t {task.models[1]} -i {source_file.id} {task.id} -c {config_file_name}"
-            ),
-        ]
-    )
+    subprocess.run(["sbatch", sbatch_file_name])
 
 
 def _generate_config_file(task: Task):
@@ -43,3 +26,35 @@ def _generate_config_file(task: Task):
         simplejson.dump(task.parameters, config_file)
 
     return config_file_name
+
+
+def _generate_sbatch_file(source_file, task: Task, config_file_name: str):
+    sbatch_file_name = f"sbatch_{task.id}"
+
+    with open(sbatch_file_name, "w", encoding="utf-8") as sbatch_file:
+        sbatch_file.writelines(
+            [
+                "#!/bin/bash\n",
+                "#SBATCH -N 1\n",
+                "#SBATCH -c 1\n",
+                "#SBATCH -p gpu\n",
+                "#SBATCH -G 1\n",
+                f"#SBATCH -J tsa_{task.id}\n",
+                f"#SBATCH -e logs/tsa_{task.id}.err\n",
+                f"#SBATCH -o logs/tsa_{task.id}.out\n",
+            ]
+        )
+        sbatch_file.write(
+            (
+                "singularity exec --bind ~/models:/app/models --nv"
+                f" --env MODELS_PATH=/app/models,DATABASE_NAME='{config.DB_NAME}',DATABASE_URL='{config.DB_URL}',"
+                f"NEPTUNE_PROJECT='ondrejpudis/diploma-thesis',NEPTUNE_API_KEY='{config.NEPTUNE_API_KEY}'"
+                " traffic-surveys-automation_master.sif"
+                f" python /app/cli/analyse.py"
+                f" -f {config.SOURCE_FILES_PATH}/{source_file.path}"
+                f" -o {config.OUTPUT_FILES_PATH}/{task.output_path}"
+                f" -d {task.models[0]} -t {task.models[1]} -i {source_file.id} {task.id} -c {config_file_name}\n"
+            )
+        )
+
+    return sbatch_file_name
